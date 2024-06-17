e046ff375a2e1b44ad02d5b74639be510d14c8d5
sistemasgrupoimagensac2 <grupoimagen.908883889@gmail.com>
2024-06-17 18:55:27 -0500

    create user

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -34,39 +34,70 @@ class LoginController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            'tipo_usuario_id' => 'required|string|max:255',
+        /* $request->validate([
+            'tipo_usuario_id' => 'required|integer|between:0,9',
             'nombres' => 'required|string|max:255',
             'apellidos' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
-            'tipo_documento_id' => 'required|string|max:255',
-            'celular' => 'required|string|max:255',
-            'imagen' => 'required|string|max:255',
-            'estado' => 'required|string|max:255',
-            'acepta_termino_condiciones' => 'required|string|max:255',
-            'acepta_confidencialidad' => 'required|string|max:255',
+            'tipo_documento_id' => 'required|integer|max:1',
+            'celular' => 'integer|digits:9',
+            'numero_documento' => 'string|max:30',
+            'imagen' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048|nullable',
+            'estado' => 'required|boolean',
+            'acepta_termino_condiciones' => 'required|boolean',
+            'acepta_confidencialidad' => 'required|boolean',
+        ]); */
+
+        $validator = Validator::make($request->all(), [
+            'tipo_usuario_id' => 'required|integer|between:0,9',
+            'codigo_unico' => 'required|string|max:255|unique:users',
+            'nombres' => 'required|string|max:255',
+            'apellidos' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+            'tipo_documento_id' => 'required|integer|max:1',
+            'celular' => 'integer|digits:9',
+            'numero_documento' => 'required|string|max:30|unique:users',
+            'imagen' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048|nullable',
+            'estado' => 'required|boolean',
+            'acepta_termino_condiciones' => 'required|boolean',
+            'acepta_confidencialidad' => 'required|boolean',
         ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Errores de validación',
+                'errors' => $validator->errors()
+            ], 422);
+        }
 
         $user = User::create([
             'tipo_usuario_id' => $request->input('tipo_usuario_id'),
-            'codigo_unico' => $request->input('nombres'),
+            'codigo_unico' => $request->input('codigo_unico'),
             'nombres' => $request->input('nombres'),
-            'apellidos' => $request->input('nombres'),
+            'apellidos' => $request->input('apellidos'),
             'email' => $request->input('email'),
             'password' => bcrypt($request->input('password')),
-            'tipo_documento_id' => $request->input('nombres'),
-            'celular' => $request->input('nombres'),
-            'imagen' => $request->input('nombres'),
-            'estado' => $request->input('nombres'),
-            'acepta_termino_condiciones' => $request->input('nombres'),
-            'acepta_confidencialidad' => $request->input('nombres'),
+            'tipo_documento_id' => $request->input('tipo_documento_id'),
+            'numero_documento' => $request->input('numero_documento'),
+            'celular' => $request->input('celular'),
+            // 'imagen' => $request->input('imagen'),
+            'estado' => 1,
+            'acepta_termino_condiciones' => true,
+            'acepta_confidencialidad' => true,
         ]);
+        if ($request->hasFile('imagen')) {
+            $imageName = time().'.'.$request->imagen->extension();  
+            $request->imagen->move(public_path('images'), $imageName);
+            $user->imagen = $imageName;
+            $user->save();
+        }
+        return response()->json(['message' => 'Usuario creado exitosamente', 'user' => $user], 201);
 
         // Puedes hacer otras operaciones, como enviar correos electrónicos de confirmación, iniciar sesión automática, etc.
 
         // Redirigir a alguna página después de la creación exitosa
-        return redirect()->route('users.index')->with('success', 'Usuario creado exitosamente.');
+        // return redirect()->route('users.index')->with('success', 'Usuario creado exitosamente.');
     }
 
     // Manejar el cierre de sesión
