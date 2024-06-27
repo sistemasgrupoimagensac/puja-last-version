b6fdd122f93ed43c3fe3478f1c4fa84f84a0a934
sistemasgrupoimagensac2 <grupoimagen.908883889@gmail.com>
2024-06-27 17:26:26 -0500

    Fix indent crear-aviso

--- routes/web.php
+++ routes/web.php
@@ -86,6 +86,13 @@ Route::post('/my-post/store', [MyPostsController::class, 'store'])->name('posts.
 // Route::get('/my-posts/{id}/edit', [MyPostsController::class, 'edit'])->name('posts.edit');
 // Route::put('/my-posts/{id}', [MyPostsController::class, 'update'])->name('posts.update');
 // Route::delete('/my-posts/{id}', [MyPostsController::class, 'destroy'])->name('posts.destroy');
+
+Route::get('/my-post/operaciones/subtipos', [MyPostsController::class, 'get_subtipos']);
+Route::get('/my-post/ubicacion/departamentos', [MyPostsController::class, 'getDepartamentos']);
+Route::get('/my-post/ubicacion/provincias/{departamentoId}', [MyPostsController::class, 'getProvincias']);
+Route::get('/my-post/ubicacion/distritos/{provinciaId}', [MyPostsController::class, 'getDistritos']);
+Route::get('/my-post/extras/{extra_id}', [MyPostsController::class, 'getExtras']);
+
 Route::get('/register', function() {
     return view('auth.register');
 });
@@ -100,24 +107,5 @@ Route::get('/proyectos', function() {
     return view('landing-projects');
 });
 
-// rutas de la creacion del aviso
-
-Route::get('/crear-aviso', [AvisoController::class, 'create'])->name('avisos.create');
-Route::post('/guardar-aviso/paso1', [AvisoController::class, 'storePaso1'])->name('avisos.store.paso1');
-Route::post('/guardar-aviso/paso2/{id}', [AvisoController::class, 'storePaso2'])->name('avisos.store.paso2');
-Route::post('/guardar-aviso/paso3/{id}', [AvisoController::class, 'storePaso3'])->name('avisos.store.paso3');
-Route::post('/guardar-aviso/paso4/{id}', [AvisoController::class, 'storePaso4'])->name('avisos.store.paso4');
-Route::post('/guardar-aviso/paso5/{id}', [AvisoController::class, 'storePaso5'])->name('avisos.store.paso5');
-Route::post('/guardar-aviso/paso6/{id}', [AvisoController::class, 'storePaso6'])->name('avisos.store.paso6');
-
-
 Route::get('/images/{archivo}', [ImagesController::class, 'get_images']);
 
-Route::get('/my-post/operaciones/subtipos', [MyPostsController::class, 'get_subtipos']);
-
-// Route::get('/my-post/ubicacion/departamentos', [MyPostsController::class, 'get_ubicacion_depa']);
-Route::get('/my-post/ubicacion/departamentos', [MyPostsController::class, 'getDepartamentos']);
-Route::get('/my-post/ubicacion/provincias/{departamentoId}', [MyPostsController::class, 'getProvincias']);
-Route::get('/my-post/ubicacion/distritos/{provinciaId}', [MyPostsController::class, 'getDistritos']);
-
-Route::get('/my-post/extras/{extra_id}', [MyPostsController::class, 'getExtras']);
