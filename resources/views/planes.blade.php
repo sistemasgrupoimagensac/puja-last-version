b7ee7b3951263f3b35346ed51135f89ad1c6ef92
walkeralfaro <dev.walkeralfaro@gmail.com>
2024-07-01 18:59:46 -0500

    walker: add planes scss

--- resources/views/planes.blade.php
+++ resources/views/planes.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.app')
 
-@section('styles')
-    
+@section('title')
+  Planes de Pago
 @endsection
+
+@push('styles')
+  @vite(['resources/sass/pages/planes.scss'])
+@endpush
 
 @section('header')
   @include('components.header')
@@ -17,44 +21,49 @@
 
 
   {{-- Cards de plans --}}
-  <div>
+  <div class="d-flex flex-column align-items-center my-5">
 
-    {{-- <div class="" role="" aria-label="Basic radio toggle button group">
-      <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
-      <label class="btn-lg btn-outline-secondary button-filter" for="btnradio1">5 Avisos</label>
-    
-      <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-      <label class="btn btn-outline-secondary button-filter" for="btnradio2">10 Avisos</label>
-    
-      <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-      <label class="btn btn-outline-secondary button-filter" for="btnradio3">25 Avisos</label>
-    </div> --}}
-
-    <div role="group" class="d-flex flex-wrap justify-content-between gap-3" x-data="{ open: false }">
+    <div role="group" class="planes-numero-avisos d-flex flex-wrap justify-content-between justify-content-lg-start w-100 gap-3 gap-lg-4 px-1 p-lg-0" style="max-width: 712px" x-data="{ open: false }">
 
       <div>
         <input type="radio" class="btn-check" name="num_avisos" id="5avisos" autocomplete="off" checked>
-        <label class="btn btn-lg btn-outline-secondary button-filter fs-2" for="5avisos" style="width: 180px">5 Avisos</label>
+        <label class="btn btn-lg btn-outline-secondary button-filter fs-2 px-0 py-3" for="5avisos" style="width: 160px">5 Avisos</label>
       </div>
       <div>
         <input type="radio" class="btn-check" name="num_avisos" id="10avisos" autocomplete="off">
-        <label class="btn btn-lg btn-outline-secondary button-filter fs-2" for="10avisos" style="width: 185px">10 Avisos</label>
+        <label class="btn btn-lg btn-outline-secondary button-filter fs-2 px-0 py-3" for="10avisos" style="width: 160px">10 Avisos</label>
       </div>
       <div>
         <input type="radio" class="btn-check" name="num_avisos" id="25avisos" autocomplete="off">
-        <label class="btn btn-lg btn-outline-secondary button-filter fs-2" for="25avisos" style="width: 185px">25 Avisos</label>
-      </div>
-      <div>
-        <input type="radio" class="btn-check" name="num_avisos" id="50avisos" autocomplete="off">
-        <label class="btn btn-lg btn-outline-secondary button-filter fs-2" for="50avisos" style="width: 185px">50 Avisos</label>
-      </div>
-      {{-- <div class="text-center"> --}}
-      <button style="width: 185px" class="btn icon-orange" :click="open = ! open" >mostrar más</button>
-      {{-- </div> --}}
+        <label class="btn btn-lg btn-outline-secondary button-filter fs-2 px-0 py-3" for="25avisos" style="width: 160px">25 Avisos</label>
+      </div> 
       <div x-show="open" x-transition>
+        <input type="radio" class="btn-check" name="num_avisos" id="50avisos" autocomplete="off">
+        <label class="btn btn-lg btn-outline-secondary button-filter fs-2 px-0 py-3" for="50avisos" style="width: 160px">50 Avisos</label>
+      </div>
+      <div x-show="open" x-transition>
+        <input type="radio" class="btn-check" name="num_avisos" id="75avisos" autocomplete="off">
+        <label class="btn btn-lg btn-outline-secondary button-filter fs-2 px-0 py-3" for="75avisos" style="width: 160px">75 Avisos</label>
+      </div>
+      <div x-show="open" x-transition>
+        <input type="radio" class="btn-check" name="num_avisos" id="100avisos" autocomplete="off">
+        <label class="btn btn-lg btn-outline-secondary button-filter fs-2 px-0 py-3" for="100avisos" style="width: 160px">100 Avisos</label>
+      </div>
+      <div x-show="open" x-transition>
+        <input type="radio" class="btn-check" name="num_avisos" id="200avisos" autocomplete="off">
+        <label class="btn btn-lg btn-outline-secondary button-filter fs-2 px-0 py-3" for="200avisos" style="width: 160px">200 Avisos</label>
+      </div>
+
+      <button class="btn icon-orange" x-on:click="open = ! open" style="width: 160px" x-show="!open" x-transition>mostrar más</button>
+      <button class="btn icon-orange" x-on:click="open = ! open" style="width: 160px" x-show="open" x-transition>mostrar menos</button>
+      {{-- <div class="text-center"> --}}
+      {{-- <button style="width: 185px" class="btn icon-orange" x-on:click="open = ! open" x-show="!open" x-transition>mostrar más</button>
+      <button style="width: 185px" class="btn icon-orange" x-on:click="open = ! open" x-show="open" x-transition>mostrar menos</button> --}}
+      {{-- </div> --}}
+      {{-- <div x-show="open" x-transition>
         <div>
           <input type="radio" class="btn-check" name="num_avisos" id="75avisos" autocomplete="off">
-          <label class="btn btn-lg btn-outline-secondary button-filter fs-2" for="75avisos" style="width: 185px">75 Avisos</label>
+          <label class="btn btn-lg btn-outline-secondary button-filter fs-3" for="75avisos" style="width: 185px">75 Avisos</label>
         </div>
         <div>
           <input type="radio" class="btn-check" name="num_avisos" id="100avisos" autocomplete="off">
@@ -64,26 +73,19 @@
           <input type="radio" class="btn-check" name="num_avisos" id="200avisos" autocomplete="off">
           <label class="btn btn-lg btn-outline-secondary button-filter fs-2" for="200avisos" style="width: 185px">200 Avisos</label>
         </div>
-      </div>
+      </div> --}}
 
-      <button style="width: 185px" class="btn icon-orange">mostrar menos</button>
+      {{-- <button style="width: 185px" class="btn icon-orange">mostrar menos</button> --}}
 
 
     </div>
-    
-
-    <x-card-plan></x-card-plan>
-
-    <div x-data="{ open: false }">
-      <button x-on:click="open = ! open">Toggle Dropdown</button>
-   
-      <div x-show="open" x-transition>
-          Dropdown Contents...
-      </div>
-  </div>
+  
 
 
   </div>
+
+  <x-card-plan></x-card-plan>
+
 
 
 
