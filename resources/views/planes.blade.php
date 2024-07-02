4612662eff043f13a43d4d059311d476fa3fba37
walkeralfaro <dev.walkeralfaro@gmail.com>
2024-07-02 18:18:22 -0500

    walker: continue planes

--- resources/views/planes.blade.php
+++ resources/views/planes.blade.php
@@ -17,74 +17,134 @@
 
 <div class="container my-5">
 
-  <h1 class="text-center icon-orange">Elige tu plan</h1>
+  <h1 class="text-center fw-bold">Elige tu plan</h1>
 
 
-  {{-- Cards de plans --}}
-  <div class="d-flex flex-column align-items-center my-5">
+  {{-- Planes --}}
+  <form>
+    @csrf
+    <div class="d-flex flex-column align-items-center my-5 gap-5">
 
-    <div role="group" class="planes-numero-avisos d-flex flex-wrap justify-content-between justify-content-lg-start w-100 gap-3 gap-lg-4 px-1 p-lg-0" style="max-width: 712px" x-data="{ open: false }">
+      {{-- numero de avisos --}}
+      <fieldset>
+        <legend class=" text-center text-secondary h5">1. Selecciona el número de avisos.</legend>
 
-      <div>
-        <input type="radio" class="btn-check" name="num_avisos" id="5avisos" autocomplete="off" checked>
-        <label class="btn btn-lg btn-outline-secondary button-filter fs-2 px-0 py-3" for="5avisos" style="width: 160px">5 Avisos</label>
-      </div>
-      <div>
-        <input type="radio" class="btn-check" name="num_avisos" id="10avisos" autocomplete="off">
-        <label class="btn btn-lg btn-outline-secondary button-filter fs-2 px-0 py-3" for="10avisos" style="width: 160px">10 Avisos</label>
-      </div>
-      <div>
-        <input type="radio" class="btn-check" name="num_avisos" id="25avisos" autocomplete="off">
-        <label class="btn btn-lg btn-outline-secondary button-filter fs-2 px-0 py-3" for="25avisos" style="width: 160px">25 Avisos</label>
-      </div> 
-      <div x-show="open" x-transition>
-        <input type="radio" class="btn-check" name="num_avisos" id="50avisos" autocomplete="off">
-        <label class="btn btn-lg btn-outline-secondary button-filter fs-2 px-0 py-3" for="50avisos" style="width: 160px">50 Avisos</label>
-      </div>
-      <div x-show="open" x-transition>
-        <input type="radio" class="btn-check" name="num_avisos" id="75avisos" autocomplete="off">
-        <label class="btn btn-lg btn-outline-secondary button-filter fs-2 px-0 py-3" for="75avisos" style="width: 160px">75 Avisos</label>
-      </div>
-      <div x-show="open" x-transition>
-        <input type="radio" class="btn-check" name="num_avisos" id="100avisos" autocomplete="off">
-        <label class="btn btn-lg btn-outline-secondary button-filter fs-2 px-0 py-3" for="100avisos" style="width: 160px">100 Avisos</label>
-      </div>
-      <div x-show="open" x-transition>
-        <input type="radio" class="btn-check" name="num_avisos" id="200avisos" autocomplete="off">
-        <label class="btn btn-lg btn-outline-secondary button-filter fs-2 px-0 py-3" for="200avisos" style="width: 160px">200 Avisos</label>
-      </div>
-
-      <button class="btn icon-orange" x-on:click="open = ! open" style="width: 160px" x-show="!open" x-transition>mostrar más</button>
-      <button class="btn icon-orange" x-on:click="open = ! open" style="width: 160px" x-show="open" x-transition>mostrar menos</button>
-      {{-- <div class="text-center"> --}}
-      {{-- <button style="width: 185px" class="btn icon-orange" x-on:click="open = ! open" x-show="!open" x-transition>mostrar más</button>
-      <button style="width: 185px" class="btn icon-orange" x-on:click="open = ! open" x-show="open" x-transition>mostrar menos</button> --}}
-      {{-- </div> --}}
-      {{-- <div x-show="open" x-transition>
-        <div>
-          <input type="radio" class="btn-check" name="num_avisos" id="75avisos" autocomplete="off">
-          <label class="btn btn-lg btn-outline-secondary button-filter fs-3" for="75avisos" style="width: 185px">75 Avisos</label>
+        <div role="group" class="planes-numero-avisos d-flex flex-wrap w-100 gap-3 gap-lg-4 px-1 p-lg-0 mt-4" x-data="{ open: false }">
+          <div>
+            <input type="radio" class="btn-check" name="num_avisos" id="5avisos" value="5" autocomplete="off" checked>
+            <label class="btn btn-lg btn-outline-primary button-filter fs-2 px-0 py-3" for="5avisos">5 Avisos</label>
+          </div>
+          <div>
+            <input type="radio" class="btn-check" name="num_avisos" id="10avisos"  value="10" autocomplete="off">
+            <label class="btn btn-lg btn-outline-primary button-filter fs-2 px-0 py-3" for="10avisos">10 Avisos</label>
+          </div>
+          <div>
+            <input type="radio" class="btn-check" name="num_avisos" id="25avisos"  value="25" autocomplete="off">
+            <label class="btn btn-lg btn-outline-primary button-filter fs-2 px-0 py-3" for="25avisos">25 Avisos</label>
+          </div> 
+          <div x-show="open" x-transition.duration.200ms>
+            <input type="radio" class="btn-check" name="num_avisos" id="50avisos"  value="50" autocomplete="off">
+            <label class="btn btn-lg btn-outline-primary button-filter fs-2 px-0 py-3" for="50avisos">50 Avisos</label>
+          </div>
+          <div x-show="open" x-transition.duration.200ms>
+            <input type="radio" class="btn-check" name="num_avisos" id="75avisos"  value="75" autocomplete="off">
+            <label class="btn btn-lg btn-outline-primary button-filter fs-2 px-0 py-3" for="75avisos">75 Avisos</label>
+          </div>
+          <div x-show="open" x-transition.duration.200ms>
+            <input type="radio" class="btn-check" name="num_avisos" id="100avisos"  value="100" autocomplete="off">
+            <label class="btn btn-lg btn-outline-primary button-filter fs-2 px-0 py-3" for="100avisos">100 Avisos</label>
+          </div>
+          <div x-show="open" x-transition.duration.200ms>
+            <input type="radio" class="btn-check" name="num_avisos" id="200avisos"  value="200" autocomplete="off">
+            <label class="btn btn-lg btn-outline-primary button-filter fs-2 px-0 py-3" for="200avisos">200 Avisos</label>
+          </div>
+    
+          <div class="avisos-desplegar d-flex align-items-center justify-content-center icon-orange border rounded" x-on:click="open = ! open">
+            <p class="m-0" x-show="!open">mostrar más</p>
+            <p class="m-0" x-show="open">mostrar menos</p>
+          </div>
         </div>
-        <div>
-          <input type="radio" class="btn-check" name="num_avisos" id="100avisos" autocomplete="off">
-          <label class="btn btn-lg btn-outline-secondary button-filter fs-2" for="100avisos" style="width: 185px">100 Avisos</label>
+      </fieldset>
+
+      <fieldset>
+        <legend class="h5 text-secondary">2. Elige el tiempo de duración de tu plan.</legend>
+
+        <div role="group" class="planes-numero-avisos d-flex flex-wrap w-100 gap-3 gap-lg-4 px-1 p-lg-0 mt-4" x-data="{ open: false }">
+          <div>
+            <input type="radio" class="btn-check" name="periodo_plan" id="90" value="1" autocomplete="off" checked>
+            <label class="btn btn-lg btn-outline-secondary fs-3 px-0 py-1" for="90">90 días</label>
+          </div>
+          <div>
+            <input type="radio" class="btn-check" name="periodo_plan" id="180"  value="2" autocomplete="off">
+            <label class="btn btn-lg btn-outline-secondary fs-3 px-0 py-1" for="180">180 días</label>
+          </div>
+          <div>
+            <input type="radio" class="btn-check" name="periodo_plan" id="365"  value="3" autocomplete="off">
+            <label class="btn btn-lg btn-outline-secondary fs-3 px-0 py-1" for="365">365 días</label>
+          </div> 
+
         </div>
-        <div>
-          <input type="radio" class="btn-check" name="num_avisos" id="200avisos" autocomplete="off">
-          <label class="btn btn-lg btn-outline-secondary button-filter fs-2" for="200avisos" style="width: 185px">200 Avisos</label>
-        </div>
-      </div> --}}
 
-      {{-- <button style="width: 185px" class="btn icon-orange">mostrar menos</button> --}}
-
-
-    </div>
+      </fieldset>
   
+      {{-- categoria de plan --}}
+      <fieldset>
+        <legend class="text-center text-secondary h5">3. Selecciona el plan que se ajusta a tus necesidades.</legend>
+        
+        <div role="group" class="d-flex flex-column flex-lg-row gap-3 mt-4">
+          {{-- plan basico --}}
+          <div>
+            <input type="radio" class="btn-check plan-btn-basic" name="type_plan" id="basic_plan" value="basic_plan" autocomplete="off" checked>
+            <x-card-plan
+              title="Básico"
+
+              price="259"
+              time="90"
+
+              text="esto es lo minimo"
+              plan="basic_plan"
+              className="btn-light border-dark-subtle"
+            />
+        
+          </div>
+    
+          {{-- plan estandard --}}
+          <div>
+            <input type="radio" class="btn-check" name="type_plan" id="standard_plan" value="standard_plan" autocomplete="off">
+            <x-card-plan 
+              title="Estándar" 
+
+              price="325"
+              time="90"
+
+              text="esto es lo adicional" 
+              plan="standard_plan"
+              className="btn-warning border-warning"
+            />
+          </div>
+    
+          {{-- plan superior --}}
+          <div>
+            <input type="radio" class="btn-check" name="type_plan" id="advance_plan" value="advance_plan" autocomplete="off">
+            <x-card-plan 
+              title="Superior" 
+              price="405"
+              time="90"
+              text="ofrece todo esto" 
+              plan="advance_plan"
+              className="btn-success border-success"
+            />
+          </div>
+
+        </div>
+      </fieldset>
+
+      
+    </div>
+  </form>
 
 
-  </div>
 
-  <x-card-plan></x-card-plan>
 
 
 
