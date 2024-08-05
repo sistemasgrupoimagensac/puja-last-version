f1f088e0d746e66102811888e291484d3b1a56a4
walkeralfaro <dev.walkeralfaro@gmail.com>
2024-08-05 08:47:07 -0500

    walker: merge 05082024

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,10 +12,6 @@ import '@fortawesome/fontawesome-free/js/all.js'
 
 // Inicializar tooltips
 document.addEventListener('DOMContentLoaded', function () {
-    /* var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl);
-    }); */
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 });
@@ -26,6 +22,3 @@ document.querySelectorAll('.custom-accordion-button').forEach((button) => {
         button.classList.add('collapsed')
     }
 })
-
-// const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-// const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
