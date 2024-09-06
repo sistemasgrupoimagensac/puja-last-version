2ff3dde59329bc93572e9dd9f0b2e092b91b0b38
sistemasgrupoimagensac2 <grupoimagen.908883889@gmail.com>
2024-09-06 14:14:23 -0500

    fix edit ad

--- app/Http/Controllers/MyPostsController.php
+++ app/Http/Controllers/MyPostsController.php
@@ -120,11 +120,13 @@ class MyPostsController extends Controller
             "estado" => 1,
         ]);
 
-        $hist_aviso = HistorialAvisos::updateOrCreate([
-            "aviso_id" => $aviso->id,
-            ],[
-            "estado_aviso_id" => 1,
-        ]);
+        if ( count($aviso->historial) === 0 ) {
+            $hist_aviso = HistorialAvisos::updateOrCreate([
+                "aviso_id" => $aviso->id,
+                ],[
+                "estado_aviso_id" => 1,
+            ]);
+        }
         
         if ($request->principal) {
             $validator = Validator::make($request->all(), [
@@ -484,27 +486,28 @@ class MyPostsController extends Controller
                 }
             }
 
-            $hist_aviso = HistorialAvisos::updateOrCreate([
-                "aviso_id" => $aviso->id,
-                ],[
-                "estado_aviso_id" => 2,
-            ]);
+            $aviso->inmueble->principal->caracteristicas->descripcion = $aviso->inmueble->descripcion;
+            $aviso->inmueble->principal->caracteristicas->save();
 
-            if (!$hist_aviso) {
-                return response()->json([
-                    'message' => 'Falló porque no se actualizo el historial avisos',
-                    'error' => true
-                ], 422);
-            } else {
-                $aviso->inmueble->principal->caracteristicas->descripcion = $aviso->inmueble->descripcion;
-                $aviso->inmueble->principal->caracteristicas->save();
-                return response()->json([
-                    'message' => 'Registro exitoso, finalizado correcto.',
-                    'status' => 'Success',
-                    'redirect_url' => route('inmueble.single', ['inmueble' => $aviso->link()]),
-                    'error' => false
-                ], 201);
+            if ( $aviso->historial->first()->pivot->estado_aviso_id !== 3 ) {
+                $hist_aviso = HistorialAvisos::updateOrCreate([
+                    "aviso_id" => $aviso->id,
+                    ],[
+                    "estado_aviso_id" => 2,
+                ]);
+                if (!$hist_aviso) {
+                    return response()->json([
+                        'message' => 'Falló porque no se actualizo el historial avisos',
+                        'error' => true
+                    ], 422);
+                }
             }
+            return response()->json([
+                'message' => 'Registro exitoso, finalizado correcto.',
+                'status' => 'Success',
+                'redirect_url' => route('inmueble.single', ['inmueble' => $aviso->link()]),
+                'error' => false
+            ], 201);
         }
 
         return response()->json([
