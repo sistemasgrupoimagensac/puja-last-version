3425e8f5145937aebe799f0396516a2413107cef
walkeralfaro <dev.walkeralfaro@gmail.com>
2024-10-25 18:11:56 -0500

    walker: cronjob corregido test correo

--- app/Console/Commands/SendContractEndReminder.php
+++ app/Console/Commands/SendContractEndReminder.php
@@ -43,7 +43,7 @@ class SendContractEndReminder extends Command
             }
         }
 
-        Log::info('Cron job correo recordatorio ejecutado correctamente.');
+        Log::info('Cron job correo recordatorio ejecutado correctamente.', $fechaAviso, $clientes, $cliente->contactos);
         $this->info('Correo de recordatorio enviado a los clientes con contrato a 30 días de vencimiento.');
     }
 
