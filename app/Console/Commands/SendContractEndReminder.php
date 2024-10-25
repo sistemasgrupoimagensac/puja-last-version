bb0872f9ce209ed077b8fd313c9e37245cc32f41
walkeralfaro <dev.walkeralfaro@gmail.com>
2024-10-25 15:44:00 -0500

    walker: test 3 envio correo

--- app/Console/Commands/SendContractEndReminder.php
+++ app/Console/Commands/SendContractEndReminder.php
@@ -7,6 +7,7 @@ use App\Models\ProyectoCliente;
 use Carbon\Carbon;
 use App\Mail\ContractEndReminder;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
 
 class SendContractEndReminder extends Command
 {
@@ -16,15 +17,17 @@ class SendContractEndReminder extends Command
     public function handle()
     {
         // Fecha actual más 30 días
-        // $fechaAviso = Carbon::now()->addDays(30)->toDateString();
-        $fechaAviso = Carbon::now();
+        $fechaAviso = Carbon::now()->addDays(30)->toDateString();
 
         // Buscar clientes con fecha_fin_contrato igual a la fecha de aviso
-        $clientes = ProyectoCliente::whereDate('fecha_fin_contrato', $fechaAviso)->get();
+        // $clientes = ProyectoCliente::whereDate('fecha_fin_contrato', $fechaAviso)->get();
+        $clientes = ProyectoCliente::all();
 
         foreach ($clientes as $cliente) {
             Mail::to($cliente->email_contacto)->send(new ContractEndReminder($cliente));
         }
+
+        Log::info('Cron job ejecutado correctamente.');
 
         $this->info('Correo de recordatorio enviado a los clientes con contrato a 30 días de vencimiento.');
     }
