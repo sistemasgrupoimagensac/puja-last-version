70e6c084748a31af1d577579f812b55351a800c3
sistemasgrupoimagensac2 <grupoimagen.908883889@gmail.com>
2024-12-19 17:01:30 -0500

    Gtag add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,15 @@
 			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 			})(window,document,'script','dataLayer','GTM-TJ62TFV5');</script>
 		<!-- End Google Tag Manager -->
+
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y55C6PC8V3"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', 'G-Y55C6PC8V3');
+		</script>
 		
 		<title>Pujainmobiliaria - @yield('title')</title>
 
