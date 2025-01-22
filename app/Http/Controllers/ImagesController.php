7a80be0e2f4697a91de915b1269686094d2622d0
sistemasgrupoimagensac2 <grupoimagen.908883889@gmail.com>
2025-01-22 16:08:29 -0500

    fix imgs proyecto 4

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -70,6 +70,13 @@ class ImagesController extends Controller
         return Storage::disk('wasabi')->response($path);
     }
 
+    public function get_project_images($id_proyecto, $archivo){
+        $path = "/proyectos/images/{$id_proyecto}/{$archivo}";
+        $existe_archivo = Storage::disk('wasabi')->exists($path);
+        if (!$existe_archivo) abort(404, 'El archivo no existe');
+        return Storage::disk('wasabi')->response($path);
+    }
+
     // Rutas DEV para unidades de proyectos
     public function dev_get_project_unidad_images($name_dev, $id_proyecto, $id_unidad, $archivo){
         $path = "wsb-dev/{$name_dev}/proyectos/unidades/{$id_proyecto}/{$id_unidad}/{$archivo}";
